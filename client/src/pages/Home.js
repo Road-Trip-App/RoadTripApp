fca3f5acc125a1076f5350544416a16e3e2d53d6
AliciaJK <alicia.krolak4@gmail.com>
2021-07-29 19:09:34 -0400

    npm i

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import NewTripForm  from '../components/NewTripForm/NewTripForm';
+import TripCard  from '../components/TripCard/TripCard';
+import WaypointForm  from '../components/WaypointForm/WaypointForm';
 
 const Home = () => {
   return (
     <div>
        <NewTripForm/>
+       <TripCard/>
+       <WaypointForm/>
       hello
     </div>
   )
