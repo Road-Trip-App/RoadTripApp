203f8df39ef349ca14e235013846a7598f795a56
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-08-01 16:41:14 -0400

    getting a pull

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,4 +7,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/road-trip", {
   useFindAndModify: false,
 });
 
+connection.once("open", function() {
+  console.log("Connection with MongoDB was successful");
+});
+
 module.exports = mongoose.connection;
