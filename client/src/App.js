ede5100dd04b2a0e9c1ffca3ac4c0b4335d3e4d1
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-08-03 20:29:13 -0400

    changes to weather app

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,42 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-
-
-import Header from './components/Header';
-import NewTrip from './components/TripCard/TripCard';
-import Footer from './components/Footer';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Signup from './pages/Signup';
-import MapContainer from './pages/MapContainer';
-import NewTripBtn from './components/NewTripBtn/NewTripBtn';
-import NewTripForm from './components/NewTripForm/NewTripForm';
-import WayPointForm from './components/WaypointForm/WaypointForm';
-import PinCard from './components/PinCard/PinCard';
-import NewPinBtn from './components/NewPinBtn/NewPinBtn';
-import TripCard from './components/TripCard/TripCard';
-import Auth from './utils/auth';
+import Header from "./components/Header";
+import NewTrip from "./components/TripCard/TripCard";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import MapContainer from "./pages/MapContainer";
+import NewTripBtn from "./components/NewTripBtn/NewTripBtn";
+import NewTripForm from "./components/NewTripForm/NewTripForm";
+import WayPointForm from "./components/WaypointForm/WaypointForm";
+import PinCard from "./components/PinCard/PinCard";
+import NewPinBtn from "./components/NewPinBtn/NewPinBtn";
+import TripCard from "./components/TripCard/TripCard";
+import Auth from "./utils/auth";
 import Forecast from "./components/Forecast/Forecast";
 
-export const weatherApiKey = process.env.REACT_APP_WEATHER_API
+export const weatherApiKey = process.env.REACT_APP_WEATHER_API;
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -52,64 +50,59 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-flex-start min-400-vh w-full bg-blue spacing">
-          <Header />
-          <div className="container">
-          {!Auth.loggedIn() ? 
-          <>
-            <Route path='/'>
-              <Login/>
-            </Route>  
-
-            <Route path='/signup'>
-              <Signup/>
+        {/* <div className="flex-column justify-flex-start min-400-vh w-full bg-blue spacing"> */}
+        <Header />
+        {/* <div className="container"> */}
+        {/* {!Auth.loggedIn() ? (
+          <> */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/login">
+              <Login />
             </Route>
 
-            </>
-            :
-            <>
-            <Route path='/'>
-              <Home /> 
+            <Route exact path="/signup">
+              <Signup />
             </Route>
-        
+          {/* </>
+        ) : (
+          <> */}
+            {/* <Route exact path="/login">
+              <Login />
+            </Route>
+
+            <Route exact path="/">
+              <Home />
+            </Route> */}
+
             <Route exact path="/dashboard">
-             <Dashboard />
-             <Forecast />
-
-             
+              <Dashboard />
+              <Forecast />
             </Route>
 
-
-
-
-
-
-            
             <Route exact path="/new-trip">
               <NewTrip />
             </Route>
 
-
             <Route exact path="/SingleTripView">
               <MapContainer />
               <NewPinBtn />
-              <PinCard/>
-
+              <PinCard />
             </Route>
             {/* silly test path */}
             <Route exact path="/banana">
               <NewTripForm />
             </Route>
 
-
             <Route exact path="/way-point">
               <WayPointForm />
             </Route>
-            </>
-          }
-          </div>
-          <Footer />
-        </div>
+          {/* </>
+        )} */}
+        {/* </div> */}
+        <Footer />
+        {/* </div> */}
       </Router>
     </ApolloProvider>
   );
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
-import './login.css'
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import "./login.css";
+import Auth from "../utils/auth";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
+  // accessing brower history
+  const history = useHistory();
 
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
 
     setFormState({
-      
       ...formState,
       [name]: value,
-    
     });
   };
   // submit form
@@ -30,67 +30,77 @@ const Login = (props) => {
       });
 
       Auth.login(data.login.token);
+
+      // if we are logged in
+      if (data) {
+        // we are redirecting (fingers crossed) to dashboard
+        return history.push("/dashboard");
+      }
     } catch (e) {
       console.error(e);
     }
 
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
 
   return (
     <main className="row login">
       <div className="col">
-        <div className="card" style={{ width: '18rem' }}>
-        <div className="card-title">
-          <h4 className="page-title text-dark">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/dashboard">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                 <label for="email" class="form-label">Email:</label>
-                <input
-                  className="form-input"
-                  placeholder="name@example.com"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <label for="password-signup" class="form-label">Password:</label>
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <div className="form-group">
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-                </div>
-              </form>
-            )}
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-title">
+            <h4 className="page-title text-dark">Login</h4>
+            <div className="card-body">
+              {data ? (
+                <p>
+                  Success! You may now head{" "}
+                  <Link to="/dashboard">back to the homepage.</Link>
+                </p>
+              ) : (
+                <form onSubmit={handleFormSubmit}>
+                  <label for="email" class="form-label">
+                    Email:
+                  </label>
+                  <input
+                    className="form-input"
+                    placeholder="name@example.com"
+                    name="email"
+                    type="email"
+                    value={formState.email}
+                    onChange={handleChange}
+                  />
+                  <label for="password-signup" class="form-label">
+                    Password:
+                  </label>
+                  <input
+                    className="form-input"
+                    placeholder="******"
+                    name="password"
+                    type="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                  />
+                  <div className="form-group">
+                    <button
+                      className="btn btn-block btn-primary"
+                      style={{ cursor: "pointer" }}
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              )}
 
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
+              {error && (
+                <div className="my-3 p-3 bg-danger text-white">
+                  {error.message}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
