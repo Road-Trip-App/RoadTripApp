4eed19d98c3e83f0a5a5818ca58b5928055e7522
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-08-04 20:46:55 -0400

    updated styling to the calander

--- client/src/components/Calandar/Calandar.js
+++ client/src/components/Calandar/Calandar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const Calandar = () => {
   const [date, setDate] = useState(new Date());
@@ -8,7 +9,7 @@ const Calandar = () => {
     setDate(date);
   };
   return (
-    <div>
+    <div className="d-flex justify-content-center">
       <Calendar onChange={onChange} value={date} />
     </div>
   );
